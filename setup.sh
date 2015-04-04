912cd47adff8d744e87f52e967c1dd63fb2607b1
Ashlynn <ashlynn.wonderland@gmail.com>
2015-04-04 17:09:30 -0400

    Fixed setup script

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ mkdir ~/.zsh-powerline
 git clone https://github.com/AshlynnInWonderland/zsh-powerline.git ~/.zsh-powerline/zsh-powerline
 cp -r ~/.zsh-powerline/zsh-powerline/default/* ~/.zsh-powerline
 cp ~/.zsh-powerline/zsh-powerline/zline.sh ~/.zsh-powerline
-echo "Add the following like to your .zshrc to finish installing:\nsource ~/.zsh-powerline/zline.sh"
+echo "Add the following like to your .zshrc to finish installing:"
+echo "source ~/.zsh-powerline/zline.sh"
